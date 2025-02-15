678ca82de18703c1791a5d1326096ede1f2eb999
aparnabhaasi <aparnabhaasi@gmail.com>
2025-02-15 17:57:05 +0530

    Twelth Commits

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions";
-
-const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
+export const config = {
+    runtime: "edge",
+  };
+  
+  import NextAuth from "next-auth";
+  import { authOptions } from "@/lib/authOptions";
+  
+  const handler = NextAuth(authOptions);
+  
+  export { handler as GET, handler as POST };
+  
