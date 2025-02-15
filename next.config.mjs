c86619347bd52111bcb9077dcd128acb4c5beead
aparnabhaasi <aparnabhaasi@gmail.com>
2025-02-15 16:07:37 +0530

    Tenth Commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    warnAfterDeprecated: false,
-  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve('./src');
+    return config;
+  }
 };
 
 export default nextConfig;
