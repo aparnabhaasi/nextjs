4932fa9a72b1d7905be7420583cce08e3bca362c
aparnabhaasi <aparnabhaasi@gmail.com>
2025-02-15 17:12:53 +0530

    Eleventh Commitss

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,11 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Ensure App Router is enabled
-  },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve('./src');
     return config;
-  },
-  redirects: async () => [
-    {
-      source: '/',
-      destination: '/auth/sign-in',
-      permanent: true,
-    },
-  ],
+  }
 };
-
 export default nextConfig;
+
