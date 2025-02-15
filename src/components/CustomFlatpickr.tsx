41694d65ba1be6dc13fe1706a3f2a70580ccc1ff
aparnabhaasi <aparnabhaasi@gmail.com>
2025-02-15 11:10:18 +0530

    third committsss

--- src/components/CustomFlatpickr.tsx
+++ src/components/CustomFlatpickr.tsx
@@ -1,19 +1,26 @@
 'use client'
+import React from 'react'
 import Flatpickr from 'react-flatpickr'
 import 'flatpickr/dist/themes/light.css'
 
 type FlatpickrProps = {
   className?: string
-  value?: Date | [Date, Date]
-  options?: {}
+  value?: Date | Date[] | string
+  options?: Record<string, any>
   placeholder?: string
+  onChange?: (selectedDates: Date[], dateStr: string, instance: any) => void
 }
 
-const CustomFlatpickr = ({ className, value, options, placeholder }: FlatpickrProps) => {
+const CustomFlatpickr: React.FC<FlatpickrProps> = ({ className, value, options, placeholder, onChange }) => {
   return (
-    <>
-      <Flatpickr className={className} data-enable-time value={value} options={options} placeholder={placeholder} />
-    </>
+    <Flatpickr
+      className={className}
+      data-enable-time
+      value={value}
+      options={options}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
   )
 }
 
